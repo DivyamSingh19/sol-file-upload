341911eca2f625887bf6da85eb51c3ddeae27e0c
Divyam Singh <divyamsingh1918@gmail.com>
2025-10-26 12:49:39 +0530

    final-fix

--- client/app/create/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useWeb3 } from '@/context/Web3Context'
-import Create from '@/components/Create'
-
-export default function CreatePage() {
-  const { marketplace, nft } = useWeb3()
-  return <Create marketplace={marketplace} nft={nft} />
-}
--- client/app/my-items/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useWeb3 } from '@/context/web3Context'
-import MyListedItems from '@/components/MyListedItems'
-
-export default function MyListedItemsPage() {
-  const { marketplace, nft, account } = useWeb3()
-  return <MyListedItems marketplace={marketplace} nft={nft} account={account} />
-}
